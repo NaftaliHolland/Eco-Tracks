0ddc526ec930c2bd7ed9f1f84e69be6a242ab510
Naftali Holland <naftaliholland01@gmail.com>
2024-02-22 10:59:56 +0300

    Removed set_env_variables.sh

--- set_env_variables.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Sets environment variables for the project
-
-export ECO_MYSQL_USER=eco_user
-export ECO_MYSQL_PWD=eco_user_pwd
-export ECO_MYSQL_HOST=localhost
-export ECO_MYSQL_DB=eco_test_db
-export ECO_ENV=dev
